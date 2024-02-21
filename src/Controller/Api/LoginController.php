8bd9f26fb04fe82f1127355a88e54025197c0707
VIRGINIE  ROSA_ARSENEV <virginieralg@gmail.com>
2024-02-21 08:52:14 +0100

    modif on security and controllers

---
+++ src/Controller/Api/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class LoginController extends AbstractController
+{
+    /**
+     * @return JsonResponse*
+     */
+    #[Route(path:"/api/login", name: "api_login", methods: ["POST"])]
+    public function ApiLogin(){
+        $user = $this->getUser();
+
+        $userData = [
+            'email'=>$user->getEmail(),
+            'name'=>$user->getName(),
+
+        ];
+        /*return new JsonResponse(json_encode($userData, JSON_THROW_ON_ERROR));
+        Pour retourner une réponse plus lisible dans le test connexion de l'API j'appelle, on retourne $this->json
+        qui convertit directement notre tableau un json.
+        */
+        return $this->json($userData);
+
+    }
+}
+
